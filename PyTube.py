57358cc203a44a314401a1a0e34214297a88aae4
keetane <yu.takahashi.k@gmail.com>
2020-09-19 02:54:20 +0900

    PyTube update

--- PyTube.py
+++ PyTube.py
@@ -2,5 +2,5 @@ from pytube import YouTube
  
 url = input("Enter the YouTube URL : ")
 print("now downloading...")
-YouTube(url).streams.get_highest_resolution().download('./Downloads/YouTube')
+YouTube(url).streams.get_highest_resolution().download('/Users/keetane/Downloads/YouTube')
 print("fertig")

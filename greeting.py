3346c0badb75626a119171e8e8b0c9582e271745
keetane <yu.takahashi.k@gmail.com>
2020-11-24 22:27:58 +0900

    updated Greetings

--- greeting.py
+++ greeting.py
@@ -1,9 +1,30 @@
-Keyword = input("What's Up?")
+#!/usr/bin/env python
+# coding: utf-8
 
-if Keyword == "Hey, What's Up?":
+# In[1]:
+
+
+Keyword = input("What's Up? \n Great > 1 \n not bad > 2 \n terrible... > 3 \n none of your business..! > other keys \n your answer > ")
+
+if Keyword == "1":
     result = 'Do you have a good news today?'
-elif Keyword == "Hi, How's your going?":
-    result = nice
-else : result = "OK"
+elif Keyword == "2":
+    result = "nice"
+elif Keyword == "3":
+    result = "have a mercy"
+else : result = "Take it easy"
 
 print(result)
+
+
+# In[2]:
+
+
+# !jupyter nbconvert --to python Greeting.ipynb
+
+
+# In[ ]:
+
+
+
+
